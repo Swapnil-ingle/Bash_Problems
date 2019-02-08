323e9384e647e16f213a9d628e4d0d16063bbfa1
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2019-02-08 13:35:06 +0530

    Added a bash script that computes the biggest of the three input integers. Input is taken from command line argument

---
+++ find-big.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+big() {
+if [[ $1 -gt $2 && $1 -gt $3 ]]
+then
+        echo  "$1 is greater.."
+elif [ $2 -gt $3 ]
+then
+        echo "$2 is greater"
+else echo "$3 is greater"
+fi
+}
+big $1 $2 $3;
